bddc6b118b5bc17f475e1bf41d541169f3c3d29a
Anoop T (DfE) <anoop.tatti@gmail.com>
2025-06-04 15:42:39 +0100

    feat: fix search test

--- tests/cypress/API/04 - Application/SearchApplication.cy.ts
+++ tests/cypress/API/04 - Application/SearchApplication.cy.ts
@@ -8,10 +8,10 @@ import { ApplicationData } from '../../support/interfaces';
 describe('Search Application', () => {
 
     const body = {
+
+        pageNumber: 1,
+        pageSize: 50000,
         data: {
-            pageNumber: 1,
-            pageSize: 50000,
-            data: {
             localAuthority: null,
             establishment: 123456,
             status: null,
@@ -21,35 +21,35 @@ describe('Search Application', () => {
             parentDateOfBirth: null,
             childLastName: null,
             childDateOfBirth: null
-            }
         }
+
     }
 
     const expectedApplicationsData: ApplicationData[] = [
         {
-          id: "bf96e60e-2030-4682-9742-0bd97787d6e2",
-          reference: "62719512",
-          establishment: {
-            id: 100020,
-            name: "Primrose Hill School",
-            localAuthority: {
-              id: 202,
-              name: "Camden"
-            }
-          },
-          parentFirstName: "Homer",
-          parentLastName: "Simpson",
-          parentNationalInsuranceNumber: "AB123456C",
-          parentNationalAsylumSeekerServiceNumber: "",
-          parentDateOfBirth: "1985-01-01",
-          childFirstName: "Tom",
-          childLastName: "sdf",
-          childDateOfBirth: "2001-01-01",
-          status: "Open",
-          user: null
+            id: "bf96e60e-2030-4682-9742-0bd97787d6e2",
+            reference: "62719512",
+            establishment: {
+                id: 100020,
+                name: "Primrose Hill School",
+                localAuthority: {
+                    id: 202,
+                    name: "Camden"
+                }
+            },
+            parentFirstName: "Homer",
+            parentLastName: "Simpson",
+            parentNationalInsuranceNumber: "AB123456C",
+            parentNationalAsylumSeekerServiceNumber: "",
+            parentDateOfBirth: "1985-01-01",
+            childFirstName: "Tom",
+            childLastName: "sdf",
+            childDateOfBirth: "2001-01-01",
+            status: "Open",
+            user: null
         }
     ];
-    
+
     it('Verify 200 Success response is returned with valid search data', () => {
 
         //Get token
