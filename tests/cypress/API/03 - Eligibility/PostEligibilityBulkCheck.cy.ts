39df71968a3b9558c9d1f00404dac61f63bbb66c
lnedeleva <lili.nedeleva@education.gov.uk>
2025-08-11 16:45:13 +0100

    add fsm type bulk checks

--- tests/cypress/API/03 - Eligibility/PostEligibilityBulkCheck.cy.ts
+++ tests/cypress/API/03 - Eligibility/PostEligibilityBulkCheck.cy.ts
@@ -1,76 +1,63 @@
 import { getandVerifyBearerToken } from '../../support/apiHelpers';
-import { invalidEligiblityCodeBulkRequestBody, validLoginRequestBody, validWorkingFamiliesBulkRequestBody,
-    invalidNinoWorkingFamiliesBulkRequestBody, invalidDobWorkingFamiliesBulkRequestBody,
-     invalidLastNameWorkingFamiliesBulkRequestBody, 
-     invalidMultiChecksWorkingFamiliesBulkRequestBody} from '../../support/requestBodies';
+import { invalidDateOfBirthBulkCheckRequestBody, validLoginRequestBody, validBulkRequestBody,
+     invalidNinoBulkRequestBody,
+     invalidLastNameBulkCheckRequestBody, 
+     invalidMultiChecksBulkRequestBody} from '../../support/requestBodies';
 
-
+const endpoints = ["free-school-meals", "early-year-pupil-premium", "two-year-offer"];
 describe('Post Eligibility Bulk Check - Valid Requests', () => {
-
-  const validWorkingFamiliesBulkRequest = validWorkingFamiliesBulkRequestBody();
-
-  it('Verify 202 Accepted response is returned with valid working families data', () => {
-    getandVerifyBearerToken('/oauth2/token', validLoginRequestBody).then((token) => {
-      cy.apiRequest('POST', 'bulk-check/working-families', validWorkingFamiliesBulkRequest, token).then((response) => {
-        // Assert the status and statusText
-        cy.verifyApiResponseCode(response, 202)
-        // Assert the response body data
-        cy.verifyPostEligibilityBulkCheckResponse(response)
+  const validBulkRequest = validBulkRequestBody();
+  endpoints.forEach((endpoint) => {
+    it(`Verify 202 Accepted response is returned with valid data for ${endpoint}`, () => {
+      getandVerifyBearerToken('/oauth2/token', validLoginRequestBody).then((token) => {
+        cy.apiRequest('POST', `bulk-check/${endpoint}`, validBulkRequest, token).then((response) => {
+          // Assert the status and statusText
+          cy.verifyApiResponseCode(response, 202)
+          // Assert the response body data
+          cy.verifyPostEligibilityBulkCheckResponse(response)
+        });
       });
     });
   });
 });
 
 describe('Post Eligibility Bulk Check - Invalid Requests', () => {
-
-  const invalidEligiblityCodeBulkRequest = invalidEligiblityCodeBulkRequestBody();
-  const invalidNinoBulkRequest = invalidNinoWorkingFamiliesBulkRequestBody();
-  const invalidDobBulkRequest = invalidDobWorkingFamiliesBulkRequestBody();
-  const invalidLastNameBulkRequest = invalidLastNameWorkingFamiliesBulkRequestBody();
-  const invalidMultiCheckBulkRequest = invalidMultiChecksWorkingFamiliesBulkRequestBody();
-
-  it('Verify 400 Bad Request response is returned with invalid Eligiblity code', () => {
-    getandVerifyBearerToken('/oauth2/token', validLoginRequestBody).then((token) => {
-      cy.apiRequest('POST', 'bulk-check/working-families', invalidEligiblityCodeBulkRequest, token).then((response) => {
-        cy.verifyApiResponseCode(response, 400)
-        expect(response.body.errors[0]).to.have.property('title', 'Item 2: Eligibility code must be 11 digits long\r\n');
+  const invalidNinoBulkRequest = invalidNinoBulkRequestBody();
+  const invalidDobBulkRequest = invalidDateOfBirthBulkCheckRequestBody();
+  const invalidLastNameBulkRequest = invalidLastNameBulkCheckRequestBody();
+  const invalidMultiCheckBulkRequest = invalidMultiChecksBulkRequestBody();
+  endpoints.forEach((endpoint) => {
+    it(`Verify 400 Bad Request response is returned with invalid National Insurance number for ${endpoint}`, () => {
+      getandVerifyBearerToken('/oauth2/token', validLoginRequestBody).then((token) => {
+        cy.apiRequest('POST', `bulk-check/${endpoint}`, invalidNinoBulkRequest, token).then((response) => {
+          cy.verifyApiResponseCode(response, 400)
+          expect(response.body.errors[0]).to.have.property('title', 'Item 1: Invalid National Insurance Number\r\n');
+        });
       });
     });
-  });
-
-  it('Verify 400 Bad Request response is returned with invalid National Insurance number', () => {
+    it(`Verify 400 Bad Request response is returned with invalid date of birth for ${endpoint}`, () => {
     getandVerifyBearerToken('/oauth2/token', validLoginRequestBody).then((token) => {
-      cy.apiRequest('POST', 'bulk-check/working-families', invalidNinoBulkRequest, token).then((response) => {
-        cy.verifyApiResponseCode(response, 400)
-        expect(response.body.errors[0]).to.have.property('title', 'Item 1: Invalid National Insurance Number\r\n');
-      });
-    });
-  });
-
-  it('Verify 400 Bad Request response is returned with invalid date of birth', () => {
-    getandVerifyBearerToken('/oauth2/token', validLoginRequestBody).then((token) => {
-      cy.apiRequest('POST', 'bulk-check/working-families', invalidDobBulkRequest, token).then((response) => {
+      cy.apiRequest('POST', `bulk-check/${endpoint}`, invalidDobBulkRequest, token).then((response) => {
         cy.verifyApiResponseCode(response, 400)
         expect(response.body.errors[0]).to.have.property('title', 'Item 2: Date of birth is required:- (yyyy-mm-dd)\r\n');
       });
     });
   });
-
-  it('Verify 400 Bad Request response is returned with invalid last name', () => {
-    getandVerifyBearerToken('/oauth2/token', validLoginRequestBody).then((token) => {
-      cy.apiRequest('POST', 'bulk-check/working-families', invalidLastNameBulkRequest, token).then((response) => {
-        cy.verifyApiResponseCode(response, 400)
-        expect(response.body.errors[0]).to.have.property('title', 'Item 2: LastName is required\r\n');
+    it(`Verify 400 Bad Request response is returned with invalid last name for ${endpoint}`, () => {
+      getandVerifyBearerToken('/oauth2/token', validLoginRequestBody).then((token) => {
+        cy.apiRequest('POST', `bulk-check/${endpoint}`, invalidLastNameBulkRequest, token).then((response) => {
+          cy.verifyApiResponseCode(response, 400)
+          expect(response.body.errors[0]).to.have.property('title', 'Item 2: LastName is required\r\n');
+        });
       });
     });
-  });
-  
-  it('Verify 400 Bad Request response is returned with multiple invalid checks', () => {
-    getandVerifyBearerToken('/oauth2/token', validLoginRequestBody).then((token) => {
-      cy.apiRequest('POST', 'bulk-check/working-families', invalidMultiCheckBulkRequest, token).then((response) => {
-        cy.verifyApiResponseCode(response, 400)
-        expect(response.body.errors[0]).to.have.property('title', 
-            'Item 1: LastName is required\r\nItem 2: Eligibility code must be 11 digits long\r\n');
+    it(`Verify 400 Bad Request response is returned with multiple invalid checks for ${endpoint}`, () => {
+      getandVerifyBearerToken('/oauth2/token', validLoginRequestBody).then((token) => {
+        cy.apiRequest('POST', `bulk-check/${endpoint}`, invalidMultiCheckBulkRequest, token).then((response) => {
+          cy.verifyApiResponseCode(response, 400)
+          expect(response.body.errors[0]).to.have.property('title', 
+              'Item 1: LastName is required\r\nItem 2: Invalid National Insurance Number\r\n');
+        });
       });
     });
   });
