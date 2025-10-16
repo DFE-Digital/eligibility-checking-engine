d7b2d11722d099d616864fe83ad25a3739dc1a96
marten-wetterberg <marten.wetterberg@nyweb.nu>
2025-10-16 10:37:29 +0100

    Add migration to remove auto-increment from Establishments

---
+++ CheckYourEligibility.API/Migrations/20251016084926_RemoveAutoIncrementFromEstablishments.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckYourEligibility.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAutoIncrementFromEstablishments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NewEstablishmentId",
+                table: "Establishments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.Sql("UPDATE Establishments SET NewEstablishmentId = EstablishmentId");
+
+            migrationBuilder.Sql(
+                "ALTER TABLE Applications DROP CONSTRAINT FK_Applications_Establishments_EstablishmentId");
+            
+            migrationBuilder.Sql(
+                "ALTER TABLE Establishments DROP CONSTRAINT PK_Establishments");
+
+            migrationBuilder.DropColumn(
+                "EstablishmentId",
+                "Establishments");
+            
+            
+            migrationBuilder.RenameColumn(
+                "NewEstablishmentId",
+                "Establishments",
+                "EstablishmentId");
+            
+            migrationBuilder.Sql("ALTER TABLE Establishments ADD CONSTRAINT PK_Establishments PRIMARY KEY (EstablishmentId)");
+            migrationBuilder.Sql("ALTER TABLE Applications ADD CONSTRAINT FK_Applications_Establishments_EstablishmentId FOREIGN KEY (EstablishmentId) REFERENCES Establishments (EstablishmentId) ON DELETE CASCADE");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
