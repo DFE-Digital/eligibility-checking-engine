740c8c31c14dfc5600a2e1e489d13b70b3959425
marten-wetterberg <marten.wetterberg@nyweb.nu>
2025-11-13 13:44:47 +0000

    Rename bulk checks ID from Group

---
+++ CheckYourEligibility.API/Migrations/20251113114411_RemoveClientIdAndRenameGuid.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckYourEligibility.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveClientIdAndRenameGuid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientIdentifier",
+                table: "BulkChecks");
+
+            migrationBuilder.RenameColumn(
+                name: "Guid",
+                table: "BulkChecks",
+                newName: "BulkCheckID");
+
+            migrationBuilder.RenameColumn(
+                name: "Group",
+                table: "EligibilityCheck",
+                newName: "BulkCheckID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "BulkChecks",
+                newName: "Guid");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientIdentifier",
+                table: "BulkChecks",
+                type: "varchar(100)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.RenameColumn(
+                name: "BulkCheckID",
+                table: "EligibilityCheck",
+                newName: "Group");
+        }
+    }
+}
