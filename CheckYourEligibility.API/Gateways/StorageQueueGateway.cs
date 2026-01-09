4b48119f9a5f87a7c0943706b07548754b8ce561
lnedeleva <lili.nedeleva@education.gov.uk>
2026-01-09 16:00:34 +0000

    logging

--- CheckYourEligibility.API/Gateways/StorageQueueGateway.cs
+++ CheckYourEligibility.API/Gateways/StorageQueueGateway.cs
@@ -1,20 +1,15 @@
 ﻿// Ignore Spelling: Fsm
 
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Text;
-using AutoMapper;
 using Azure.Storage.Queues;
 using Azure.Storage.Queues.Models;
-using CheckYourEligibility.API.Adapters;
 using CheckYourEligibility.API.Boundary.Requests;
-using CheckYourEligibility.API.Domain;
-using CheckYourEligibility.API.Domain.Constants;
 using CheckYourEligibility.API.Domain.Enums;
 using CheckYourEligibility.API.Gateways.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
 
 namespace CheckYourEligibility.API.Gateways;
 
@@ -73,6 +68,7 @@ public class StorageQueueGateway : IStorageQueue
             throw new Exception($"invalid queue {queName}.");
         
         var sw = Stopwatch.StartNew();
+        var st = Stopwatch.StartNew();
         if (await queue.ExistsAsync())
         {
             QueueProperties properties = await queue.GetPropertiesAsync();
@@ -82,6 +78,8 @@ public class StorageQueueGateway : IStorageQueue
                 QueueMessage[] retrievedMessage = await queue.ReceiveMessagesAsync(_configuration.GetValue<int>("QueueFetchSize"));
                 
                 _logger.LogInformation($"Reading queue item in {sw.ElapsedMilliseconds} ms");
+              
+                int i  = 0;
                 foreach (var item in retrievedMessage)
                 {
                     sw.Restart();
@@ -92,6 +90,7 @@ public class StorageQueueGateway : IStorageQueue
                     {
                         var postCheckAudit = await _db.Audits.FirstOrDefaultAsync(a => a.TypeID == checkData.Guid && a.Type == AuditType.Check && a.Method == "POST");
                         string scope = string.Empty;
+
                         if (postCheckAudit != null && postCheckAudit.Scope != null) scope = postCheckAudit.Scope;
 
                         var result = await _checkingEngineGateway.ProcessCheck(checkData.Guid, new AuditData
@@ -114,7 +113,7 @@ public class StorageQueueGateway : IStorageQueue
                                 await _checkEligibilityGateway.UpdateEligibilityCheckStatus(checkData.Guid,
                                     new EligibilityCheckStatusData { Status = CheckEligibilityStatus.error });
                                 await queue.DeleteMessageAsync(item.MessageId, popReceipt);
-                            }
+                            } 
                         }
                         // If status is not queued for Processing, we have a conclusive answer
                         else
@@ -149,10 +148,18 @@ public class StorageQueueGateway : IStorageQueue
                             );
                         }
                     }
-                    
+                    i++;
                     _logger.LogInformation($"Processing queue item in {sw.ElapsedMilliseconds} ms");
+                   Console.WriteLine(
+                       $"Item_No....{i}" +
+                       $"Process_Time....{sw.ElapsedMilliseconds:N0} ms");
                 }
+                Console.WriteLine(
+              $"Item_Total_Count....{i}" +
+              $"Mean....{st.ElapsedMilliseconds / i:N0} ms" +
+              $"Total_Elapsed_Time....{st.ElapsedMilliseconds:N0} ms");
             }
+          
         }
     }
 
