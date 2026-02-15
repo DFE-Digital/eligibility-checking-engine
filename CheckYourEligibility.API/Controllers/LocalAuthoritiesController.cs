599a44ed34aca142db48401450bbd29aa3465b20
GEORGANTAS, Yiannos <Ioannis.GEORGANTAS@EDUCATION.GOV.UK>
2026-02-15 13:36:08 +0000

    Added new controller file for Local Authorities

---
+++ CheckYourEligibility.API/Controllers/LocalAuthoritiesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheckYourEligibility.API.Controllers
+{
+    public class LocalAuthoritiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
