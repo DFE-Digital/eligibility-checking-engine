1da009a4f56a6a966abe6719604311d40d0eefde
GEORGANTAS, Yiannos <Ioannis.GEORGANTAS@EDUCATION.GOV.UK>
2026-02-27 23:57:40 +0000

    Extend the gateway interface

--- CheckYourEligibility.API/Gateways/Interfaces/ILocalAuthority.cs
+++ CheckYourEligibility.API/Gateways/Interfaces/ILocalAuthority.cs
@@ -5,4 +5,6 @@ namespace CheckYourEligibility.API.Gateways.Interfaces;
 public interface ILocalAuthority
 {
     Task<LocalAuthority?> GetLocalAuthorityById(int localAuthorityId);
+
+    Task<LocalAuthority?> UpdateSchoolCanReviewEvidence(int localAuthorityId, bool value);
 }
