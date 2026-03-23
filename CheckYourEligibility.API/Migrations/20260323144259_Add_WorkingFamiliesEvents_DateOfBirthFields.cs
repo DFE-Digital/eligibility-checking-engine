f57e6e59798eb652e1f862a0c0b9b52bd7457bd7
Jim Gregory <jim.gregory@advania.co.uk>
2026-03-23 14:48:48 +0000

    Add database migrations

---
+++ CheckYourEligibility.API/Migrations/20260323144259_Add_WorkingFamiliesEvents_DateOfBirthFields.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckYourEligibility.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_WorkingFamiliesEvents_DateOfBirthFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ParentDateOfBirth",
+                table: "WorkingFamiliesEvents",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PartnerDateOfBirth",
+                table: "WorkingFamiliesEvents",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentDateOfBirth",
+                table: "WorkingFamiliesEvents");
+
+            migrationBuilder.DropColumn(
+                name: "PartnerDateOfBirth",
+                table: "WorkingFamiliesEvents");
+        }
+    }
+}
