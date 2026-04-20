1f7d8831444e59a168fad1033703f97ab8574fb2
YiannosGeorgantas <ioannis.georgantas@education.gov.uk>
2026-04-20 08:48:21 +0100

    #2315 Update model class with new toggle

--- CheckYourEligibility.API/Domain/MultiAcademyTrust.cs
+++ CheckYourEligibility.API/Domain/MultiAcademyTrust.cs
@@ -12,5 +12,10 @@ public class MultiAcademyTrust
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public int MultiAcademyTrustID { get; set; }
     public string Name { get; set; }
+    /// <summary>
+    /// Indicates whether academies under this MAT are allowed to review evidence.
+    /// Overrides LA-level setting.
+    /// </summary>
+    public bool AcademyCanReviewEvidence { get; set; }
     public virtual Collection<MultiAcademyTrustEstablishment> MultiAcademyTrustEstablishments { get; set; }
 }
