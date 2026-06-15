41895fc07a29f70f6a73713d6c9f5e2b9c4b1cfb
YiannosGeorgantas <ioannis.georgantas@education.gov.uk>
2026-06-15 10:21:52 +0100

    create the new use case skeleton.

---
+++ CheckYourEligibility.API/Usecases/CreateApplicationsFromBulkCheckUseCase.cs
@@ -0,0 +1,16 @@
+﻿using CheckYourEligibility.API.Boundary.Responses;
+
+namespace CheckYourEligibility.API.UseCases;
+
+public interface ICreateApplicationsFromBulkCheckUseCase
+{
+    Task<MessageResponse> Execute(string bulkCheckId, List<int> allowedLocalAuthorityIds);
+}
+
+public class CreateApplicationsFromBulkCheckUseCase : ICreateApplicationsFromBulkCheckUseCase
+{
+    public async Task<MessageResponse> Execute(string bulkCheckId, List<int> allowedLocalAuthorityIds)
+    {
+        throw new NotImplementedException();
+    }
+}
