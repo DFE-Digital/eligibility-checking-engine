27b14babf6279cb4ea53c3a93ffc89de0652e427
YiannosGeorgantas <ioannis.georgantas@education.gov.uk>
2026-06-15 10:24:20 +0100

    Update the skeleton with dependencies and initial validation.

--- CheckYourEligibility.API/Usecases/CreateApplicationsFromBulkCheckUseCase.cs
+++ CheckYourEligibility.API/Usecases/CreateApplicationsFromBulkCheckUseCase.cs
@@ -1,4 +1,7 @@
 ﻿using CheckYourEligibility.API.Boundary.Responses;
+using CheckYourEligibility.API.Domain.Enums;
+using CheckYourEligibility.API.Domain.Exceptions;
+using Microsoft.EntityFrameworkCore;
 
 namespace CheckYourEligibility.API.UseCases;
 
@@ -9,8 +12,42 @@ public interface ICreateApplicationsFromBulkCheckUseCase
 
 public class CreateApplicationsFromBulkCheckUseCase : ICreateApplicationsFromBulkCheckUseCase
 {
+    private readonly IDbContextFactory<EligibilityCheckContext> _dbContextFactory;
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public CreateApplicationsFromBulkCheckUseCase(
+        IDbContextFactory<EligibilityCheckContext> dbContextFactory,
+        IServiceScopeFactory scopeFactory)
+    {
+        _dbContextFactory = dbContextFactory;
+        _scopeFactory = scopeFactory;
+    }
+
     public async Task<MessageResponse> Execute(string bulkCheckId, List<int> allowedLocalAuthorityIds)
     {
-        throw new NotImplementedException();
+        await using var dbContext = _dbContextFactory.CreateDbContext();
+
+        var bulkCheck = await dbContext.BulkChecks
+            .FirstOrDefaultAsync(x => x.BulkCheckID == bulkCheckId);
+
+        if (bulkCheck == null)
+        {
+            throw new NotFoundException($"Bulk check {bulkCheckId} not found");
+        }
+
+        if (bulkCheck.Status != BulkCheckStatus.Completed)
+        {
+            throw new ValidationException(
+                "Applications can only be created when bulk check status is 'Completed'");
+        }
+
+        bulkCheck.Status = BulkCheckStatus.ApplicationCreationInProgress;
+
+        await dbContext.SaveChangesAsync();
+
+        return new MessageResponse
+        {
+            Data = "Application creation started."
+        };
     }
 }
