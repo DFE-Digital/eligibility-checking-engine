66dcced58698106d4ce19944acacc26c876bbc4a
YiannosGeorgantas <ioannis.georgantas@education.gov.uk>
2026-06-16 09:29:38 +0100

    New interface

---
+++ CheckYourEligibility.API/Gateways/Interfaces/ICreateApplicationsFromBulkCheck.cs
@@ -0,0 +1,12 @@
+﻿using CheckYourEligibility.API.Domain;
+using CheckYourEligibility.API.Domain.Enums;
+
+namespace CheckYourEligibility.API.Gateways.Interfaces;
+
+public interface ICreateApplicationsFromBulkCheck
+{
+    Task<BulkCheck?> GetBulkCheck(string bulkCheckId);
+    Task<bool> HasEligibleChecks(string bulkCheckId);
+    Task<List<EligibilityCheck>> GetEligibleChecks(string bulkCheckId);
+    Task UpdateBulkCheckStatus(string bulkCheckId, BulkCheckStatus status);
+}
