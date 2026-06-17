5c22fa52cb992cac083ace699a9c1e6acaee65af
Jim Gregory <jim.gregory@advania.co.uk>
2026-06-17 13:28:33 +0100

    ELIG-3258 - Make ParentEmail optional for Applications

--- CheckYourEligibility.API/Boundary/Responses/ApplicationResponse.cs
+++ CheckYourEligibility.API/Boundary/Responses/ApplicationResponse.cs
@@ -10,7 +10,7 @@ public class ApplicationResponse
     public ApplicationEstablishment Establishment { get; set; }
     public string ParentFirstName { get; set; }
     public string ParentLastName { get; set; }
-    public string ParentEmail { get; set; }
+    public string? ParentEmail { get; set; }
     public string? ParentNationalInsuranceNumber { get; set; }
     public string? ParentNationalAsylumSeekerServiceNumber { get; set; }
     public string ParentDateOfBirth { get; set; }
