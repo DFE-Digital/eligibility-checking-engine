a8b45a2567ff5db314b8e6155d651d68730d71e2
peterbed <Peter.BEDFORD@EDUCATION.GOV.UK>
2026-06-24 11:45:30 +0100

    multiple concurrent tests, more hashed

--- tests/cypress/API/03 - Eligibility/PostEligibilityBulkCheckHashing.cy.ts
+++ tests/cypress/API/03 - Eligibility/PostEligibilityBulkCheckHashing.cy.ts
@@ -136,40 +136,26 @@ describe("Bulk Check - Hashing Behaviour", () => {
     );
   });
 
-  it("Multiple concurrent batches submitted", () => {
-    getandVerifyBearerToken("/oauth2/token", validLoginRequestBody).then(
-      (token) => {
-        cy.apiRequest(
-          "POST",
-          `bulk-check/free-school-meals`,
-          request,
-          token,
-        ).then((response1) => {
-          //  response validation
-          cy.verifyApiResponseCode(response1, 202);
-          cy.verifyPostEligibilityBulkCheckResponse(response1);
-
-          cy.apiRequest(
-            "POST",
-            `bulk-check/free-school-meals`,
-            request,
-            token,
-          ).then((response2) => {
-            cy.verifyApiResponseCode(response2, 202);
-            cy.verifyPostEligibilityBulkCheckResponse(response2);
-          });
-        });
-      },
-    );
-  });
+  //  Multiple batches
 
   it("Multiple small batches submitted concurrently", () => {
-    const small = {
+    const small1 = {
       data: [
         {
           nationalInsuranceNumber: "NN123456C",
-          lastName: "Small",
+          lastName: "Small1",
           dateOfBirth: "2001-01-01",
+          nationalAsylumSeekerServiceNumber: "",
+        },
+      ],
+    };
+
+    const small2 = {
+      data: [
+        {
+          nationalInsuranceNumber: "NN654321C",
+          lastName: "Small2",
+          dateOfBirth: "2002-02-02",
           nationalAsylumSeekerServiceNumber: "",
         },
       ],
@@ -179,8 +165,8 @@ describe("Bulk Check - Hashing Behaviour", () => {
       (token) => {
         cy.apiRequest(
           "POST",
-          `bulk-check/free-school-meals`,
-          small,
+          "bulk-check/free-school-meals",
+          small1,
           token,
         ).then((res1) => {
           cy.verifyApiResponseCode(res1, 202);
@@ -188,10 +174,11 @@ describe("Bulk Check - Hashing Behaviour", () => {
 
           const links1 = res1.body.links;
 
+          // fire second batch
           cy.apiRequest(
             "POST",
-            `bulk-check/free-school-meals`,
-            small,
+            "bulk-check/free-school-meals",
+            small2,
             token,
           ).then((res2) => {
             cy.verifyApiResponseCode(res2, 202);
@@ -199,12 +186,12 @@ describe("Bulk Check - Hashing Behaviour", () => {
 
             const links2 = res2.body.links;
 
-            //  wait for both batches
+            //  wait for both batches to complete
             cy.waitForBulkCompletion(links1.get_Progress_Check, token).then(
               () => {
                 cy.waitForBulkCompletion(links2.get_Progress_Check, token).then(
                   () => {
-                    //  fetch both results
+                    // fetch results for both
                     cy.apiRequest(
                       "GET",
                       links1.get_BulkCheck_Results,
@@ -230,6 +217,177 @@ describe("Bulk Check - Hashing Behaviour", () => {
             );
           });
         });
+      },
+    );
+  });
+
+  it("Multiple small batches submitted concurrently with mix of hashed and new checks", () => {
+    const smallBase = {
+      data: [
+        {
+          nationalInsuranceNumber: "NN123456C",
+          lastName: "Small",
+          dateOfBirth: "2001-01-01",
+          nationalAsylumSeekerServiceNumber: "",
+        },
+      ],
+    };
+
+    const mixedSmall = {
+      data: [
+        {
+          nationalInsuranceNumber: "NN123456C", // hashed
+          lastName: "Small",
+          dateOfBirth: "2001-01-01",
+          nationalAsylumSeekerServiceNumber: "",
+        },
+      ],
+    };
+
+    const newSmall = {
+      data: [
+        {
+          nationalInsuranceNumber: "NN999999C", //  new
+          lastName: "SmallNew",
+          dateOfBirth: "2005-05-05",
+          nationalAsylumSeekerServiceNumber: "",
+        },
+      ],
+    };
+
+    getandVerifyBearerToken("/oauth2/token", validLoginRequestBody).then(
+      (token) => {
+        // create hash
+        cy.apiRequest("POST", "bulk-check/free-school-meals", smallBase, token)
+          .then((res) => {
+            return cy.waitForBulkCompletion(
+              res.body.links.get_Progress_Check,
+              token,
+            );
+          })
+
+          // Concurrent second runs (mixed)
+          .then(() => {
+            cy.apiRequest(
+              "POST",
+              "bulk-check/free-school-meals",
+              mixedSmall,
+              token,
+            ).then((res1) => {
+              const links1 = res1.body.links;
+
+              cy.apiRequest(
+                "POST",
+                "bulk-check/free-school-meals",
+                newSmall,
+                token,
+              ).then((res2) => {
+                const links2 = res2.body.links;
+
+                cy.waitForBulkCompletion(links1.get_Progress_Check, token).then(
+                  () => {
+                    cy.waitForBulkCompletion(
+                      links2.get_Progress_Check,
+                      token,
+                    ).then(() => {
+                      cy.apiRequest(
+                        "GET",
+                        links1.get_BulkCheck_Results,
+                        null,
+                        token,
+                      ).then((r1) => {
+                        expect(r1.body.data.length).to.equal(1);
+                      });
+
+                      cy.apiRequest(
+                        "GET",
+                        links2.get_BulkCheck_Results,
+                        null,
+                        token,
+                      ).then((r2) => {
+                        expect(r2.body.data.length).to.equal(1);
+                      });
+                    });
+                  },
+                );
+              });
+            });
+          });
+      },
+    );
+  });
+
+  it("Multiple small batches submitted concurrently with all hashed checks", () => {
+    const small = {
+      data: [
+        {
+          nationalInsuranceNumber: "NN123456C",
+          lastName: "Small",
+          dateOfBirth: "2001-01-01",
+          nationalAsylumSeekerServiceNumber: "",
+        },
+      ],
+    };
+
+    getandVerifyBearerToken("/oauth2/token", validLoginRequestBody).then(
+      (token) => {
+        // create hash
+        cy.apiRequest("POST", "bulk-check/free-school-meals", small, token)
+          .then((res) => {
+            return cy.waitForBulkCompletion(
+              res.body.links.get_Progress_Check,
+              token,
+            );
+          })
+
+          // Concurrent hashed runs
+          .then(() => {
+            cy.apiRequest(
+              "POST",
+              "bulk-check/free-school-meals",
+              small,
+              token,
+            ).then((res1) => {
+              const links1 = res1.body.links;
+
+              cy.apiRequest(
+                "POST",
+                "bulk-check/free-school-meals",
+                small,
+                token,
+              ).then((res2) => {
+                const links2 = res2.body.links;
+
+                // wait for both
+                cy.waitForBulkCompletion(links1.get_Progress_Check, token).then(
+                  () => {
+                    cy.waitForBulkCompletion(
+                      links2.get_Progress_Check,
+                      token,
+                    ).then(() => {
+                      cy.apiRequest(
+                        "GET",
+                        links1.get_BulkCheck_Results,
+                        null,
+                        token,
+                      ).then((r1) => {
+                        expect(r1.body.data.length).to.equal(1);
+                      });
+
+                      cy.apiRequest(
+                        "GET",
+                        links2.get_BulkCheck_Results,
+                        null,
+                        token,
+                      ).then((r2) => {
+                        expect(r2.body.data.length).to.equal(1);
+                      });
+                    });
+                  },
+                );
+              });
+            });
+          });
       },
     );
   });
