980496bdf5b9db79257be418d1f737fc9fb2dcc9
YiannosGeorgantas <ioannis.georgantas@education.gov.uk>
2026-08-19 18:29:29 +0100

    ELIG-2618 Add SQL Server integration test infrastructure

---
+++ CheckYourEligibility.API.IntegrationTests/SqlServerFixture.cs
@@ -0,0 +1,51 @@
+﻿using CheckYourEligibility.API.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.MsSql;
+
+namespace CheckYourEligibility.API.IntegrationTests;
+
+[SetUpFixture]
+public sealed class SqlServerFixture
+{
+    private const string SqlServerImage =
+        "mcr.microsoft.com/mssql/server:2022-CU26-ubuntu-22.04";
+
+    private MsSqlContainer? _container;
+
+    [OneTimeSetUp]
+    public async Task StartSqlServer()
+    {
+        _container = new MsSqlBuilder()
+            .WithImage(SqlServerImage)
+            .Build();
+
+        await _container.StartAsync();
+
+        ConnectionString = _container.GetConnectionString();
+
+        await using var context = CreateContext();
+        await context.Database.MigrateAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task StopSqlServer()
+    {
+        if (_container is not null)
+        {
+            await _container.DisposeAsync();
+        }
+    }
+
+    internal static string ConnectionString { get; private set; } =
+        string.Empty;
+
+    internal static EligibilityCheckContext CreateContext()
+    {
+        var options =
+            new DbContextOptionsBuilder<EligibilityCheckContext>()
+                .UseSqlServer(ConnectionString)
+                .Options;
+
+        return new EligibilityCheckContext(options);
+    }
+}
---
+++ CheckYourEligibility.API.IntegrationTests/SqlServerMigrationTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckYourEligibility.API.IntegrationTests;
+
+[TestFixture]
+public sealed class SqlServerMigrationTests
+{
+    [Test]
+    public async Task Migrations_Create_And_Seed_EligibilityCodeRange()
+    {
+        await using var context = SqlServerFixture.CreateContext();
+
+        var range = await context.EligibilityCodeRanges.SingleAsync();
+
+        range.EligibilityCodeRangeId.Should().Be(1);
+        range.StartRange.Should().Be(40000000001);
+        range.EndRange.Should().Be(49999999999);
+        range.NextAvailableCode.Should().Be(40000000001);
+        range.RowVersion.Should().NotBeNullOrEmpty();
+    }
+}
